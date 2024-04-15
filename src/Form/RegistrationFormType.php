2bbbb0fdd9e09c0e9c2858ab2bdc8ebb92b6e1a6
stephaned7 <delaporte.stephane7@gmail.com>
2024-04-15 15:23:14 +0200

    init commit

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'E-mail'
+            ])
+            ->add('firstname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Nom de famille'
+            ])
+            ->add('birthdate', DateType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'JJ/MM/AAAA'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer une date de naissance.'
+                    ]),
+                    new Callback([
+                        'callback' => function ($value, ExecutionContextInterface $context) {
+                            if ($value instanceof \DateTime) {
+                                $formattedDate = $value->format('d/m/Y');
+                                if (!preg_match('/^\d{2}\/\d{2}\/\d{4}$/', $formattedDate)) {
+                                    $context->buildViolation('La date de naissance doit être au format JJ/MM/AAAA.')
+                                        ->addViolation();
+                                }
+                            }
+                        }
+                    ])
+                ],
+                'label' => 'Date de naissance'
+            ])
+            ->add('address', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Adresse'
+            ])
+            ->add('zipcode', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Code postal'
+            ])
+            ->add('city', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Ville'
+            ])
+            ->add('phoneNum', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Numéro de téléphone'
+            ])
+            ->add('RGPDConsent', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+                'label' => 'J\'accepte les conditions d\'utilisation de ce site',
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent être identiques.',
+                'options' => [
+                    'attr' => [
+                        'class' => 'form-control',
+                    ]
+                ],
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez entrer un mot de passe.'
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères.',
+                            'max' => 4096,
+                        ]),
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
