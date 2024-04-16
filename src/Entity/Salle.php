8521ec5f9d1551e4bf17b0ed0d5221e50d9a8c27
sooradje <soo.mauve.pro@gmail.com>
2024-04-17 00:12:46 +0200

    create entity Salle

---
+++ src/Entity/Salle.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SalleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SalleRepository::class)]
+class Salle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom_salle = null;
+
+    #[ORM\Column]
+    private ?int $capacite = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomSalle(): ?string
+    {
+        return $this->nom_salle;
+    }
+
+    public function setNomSalle(string $nom_salle): static
+    {
+        $this->nom_salle = $nom_salle;
+
+        return $this;
+    }
+
+    public function getCapacite(): ?int
+    {
+        return $this->capacite;
+    }
+
+    public function setCapacite(int $capacite): static
+    {
+        $this->capacite = $capacite;
+
+        return $this;
+    }
+}
