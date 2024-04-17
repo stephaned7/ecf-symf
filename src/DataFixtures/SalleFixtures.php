08beca9512aacdccb24ef67783b8e379d6545ab4
sooradje <soo.mauve.pro@gmail.com>
2024-04-17 02:28:23 +0200

    fixtures salles

---
+++ src/DataFixtures/SalleFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Salle;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Faker\Factory;
+
+class SalleFixtures extends Fixture
+{
+    public function __construct(private SluggerInterface $slugger)
+    {
+
+    }
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        
+        for($i = 0; $i <= 5; $i++){
+            $salle = new Salle();
+            $salle->setNomSalle($faker->lastName);
+            $salle->setCapacite($faker->numberBetween(2,10));
+            $salle->setSlug($faker->slug);
+        
+            $manager->persist($salle);
+     
+            }
+        
+        // for($salle = 0; $salle <=1; $salle++){}@
+
+        $manager->flush();
+    }
+
+}
