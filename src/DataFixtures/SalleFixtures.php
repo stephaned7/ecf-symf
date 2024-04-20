a415a69ba0e75a2aaa28c190c3e8245823148002
stephaned7 <delaporte.stephane7@gmail.com>
2024-04-20 17:34:26 +0200

    my branch

--- src/DataFixtures/SalleFixtures.php
+++ src/DataFixtures/SalleFixtures.php
@@ -2,15 +2,18 @@
 
 namespace App\DataFixtures;
 
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Faker\Factory;
 use App\Entity\Salle;
 use App\Repository\SalleRepository;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
-class SalleFixtures extends Fixture
+class SalleFixtures extends Fixture implements OrderedFixtureInterface
 {
-   
+    public function getOrder():int{
+        return 3;
+    }
     public function load(ObjectManager $manager): void
 
     {
